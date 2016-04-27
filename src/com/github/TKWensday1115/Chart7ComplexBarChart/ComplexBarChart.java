ca67b7368503e1da399e80560df7983d9140d01e
Grzegorz Najderek <najderek@student.agh.edu.pl>
2016-04-27 11:42:57 +0200

    Demo works, component modified, documentation updated

--- src/com/github/TKWensday1115/Chart7ComplexBarChart/ComplexBarChart.java
+++ src/com/github/TKWensday1115/Chart7ComplexBarChart/ComplexBarChart.java
@@ -1,8 +1,17 @@
 package com.github.TKWensday1115.Chart7ComplexBarChart;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
 
 /**
  * Created by Grzegorz on 2016-04-20.
@@ -12,24 +21,117 @@ public class ComplexBarChart extends StackedBarChart<String, Number> implements 
     private CategoryAxis xAxis;
     private NumberAxis yAxis;
 
+    private CategoriesManager categoriesManager;
+    private SeriesManager seriesManager;
+
+
     public ComplexBarChart() {
         super(new CategoryAxis(), new NumberAxis());
 
         xAxis = (CategoryAxis) super.getXAxis();
         yAxis = (NumberAxis) super.getYAxis();
 
+        categoriesManager = new CategoriesManager();
+        seriesManager = new SeriesManager();
+
         xAxis.setLabel("Time");
         yAxis.setLabel("Value");
 
+        xAxis.setCategories(categoriesManager.getCategories());  // FXCollections.<String>observableArrayList(Arrays.asList("czas1", "2", "3", "4"))
     }
 
     @Override
     public void AddData(String seriesName, String timeValue, Number number) {
 
+        categoriesManager.AddCategory(timeValue);
+
+        seriesManager.AddDataToSeries(seriesName, timeValue, number);
+
+//        ObservableList<String> newCategories = xAxis.getCategories();
+//        newCategories.add(timeValue);
+//        xAxis.setCategories(newCategories);
+
     }
 
     @Override
     public void setHistoryLength(int length) {
+        categoriesManager.setHistoryLength(length);
+    }
 
+
+    private class CategoriesManager {
+
+        private ObservableList<String> categories;
+        private int historyLength=10;
+
+        public CategoriesManager() {
+            categories = FXCollections.<String>observableArrayList(Arrays.asList());
+        }
+
+        public ObservableList<String> getCategories() {
+            return categories;
+        }
+
+        public void AddCategory(String timeValue) {
+            if (!categories.contains(timeValue)) {
+                categories.add(timeValue);
+                categories.sort(new Comparator(){
+                    public int compare(Object o1, Object o2) {
+                        return ((String)o1).compareTo(((String) o2));
+                    }
+                });
+                if(categories.size()>historyLength) {
+                    String categoryToDelete = categories.remove(0);
+                    seriesManager.DeleteCategoryFromAllSeries(categoryToDelete);
+                }
+            }
+        }
+
+        public void setHistoryLength(int historyLength) {
+            this.historyLength = historyLength;
+        }
+    }
+
+
+    private class SeriesManager {
+
+        private Map<String, XYChart.Series<String, Number>> seriesMap;
+
+        public SeriesManager() {
+            seriesMap = new HashMap<>();
+        }
+
+        public void AddDataToSeries(String seriesName, String timeValue, Number number) {
+
+            XYChart.Series<String, Number> series = getSeriesByName(seriesName);
+            series.getData().add(new XYChart.Data<String, Number>(timeValue, number));
+        }
+
+        private Series<String,Number> getSeriesByName(String seriesName) {
+            XYChart.Series<String, Number> returnSeries = seriesMap.get(seriesName);
+            if (returnSeries == null) {
+                returnSeries = new XYChart.Series<String, Number>();
+                returnSeries.setName(seriesName);
+                seriesMap.put(seriesName, returnSeries);
+                getData().add(returnSeries);
+
+//            getData().sort(new Comparator(){
+//                public int compare(Object o1, Object o2) {
+//                    return ((Series<String,Number>)o1).getName().compareTo(((Series<String,Number>)o2).getName());
+//                }
+//            });
+            }
+            return returnSeries;
+        }
+
+        public void DeleteCategoryFromAllSeries(String categoryToDelete) {
+            for (XYChart.Series<String, Number> series : seriesMap.values()) {
+                series.getData().removeIf(data -> {
+                    if(((String)(data.getXValue())).equals(categoryToDelete))
+                        return true;
+                    return false;
+                });
+            }
+        }
     }
 }
--- src/com/github/TKWensday1115/Chart7ComplexBarChart/Main.java
+++ src/com/github/TKWensday1115/Chart7ComplexBarChart/Main.java
@@ -1,6 +1,7 @@
 package com.github.TKWensday1115.Chart7ComplexBarChart;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,11 +14,69 @@ public class Main extends Application {
         stage.setTitle("Bar Chart Sample");
 
         ComplexBarChart sbc = new ComplexBarChart();
+        sbc.setHistoryLength(7);
 
         Scene scene = new Scene(sbc, 800, 600);
 //        sbc.getData().addAll(series1, series2, series3);
         stage.setScene(scene);
         stage.show();
+
+        sbc.AddData("seria1","1", 100 );
+        sbc.AddData("seria1","2", 10 );
+        sbc.AddData("seria1","4", 10 );
+        sbc.AddData("seria2","1", 60 );
+        sbc.AddData("seria2","2", 20 );
+        sbc.AddData("seria2","3", 10 );
+        sbc.AddData("seria3","3", 90 );
+        sbc.AddData("seria4","3", 40 );
+        sbc.AddData("seria1","3", 10 );
+
+        new Thread(() -> {
+
+            try {
+                Thread.sleep(2000);
+
+                Platform.runLater(() -> {
+                    sbc.AddData("seria5","4", 40 );
+                    sbc.AddData("seria5","5", 40 );
+                    sbc.AddData("seria5","6", 40 );
+                });
+
+                Thread.sleep(2000);
+
+                Platform.runLater(() -> {
+                    sbc.AddData("seria3", "1", 60);
+                    sbc.AddData("seria3", "2", 60);
+                    sbc.AddData("seria3", "5", 60);
+                });
+
+                Thread.sleep(2000);
+
+                Platform.runLater(() -> {
+                    sbc.AddData("seria6", "5", 70);
+                    sbc.AddData("seria7", "6", 70);
+                    sbc.AddData("seria6", "7", 70);
+                    sbc.AddData("seria6", "8", 70);
+                    sbc.AddData("seria6", "9", 70);
+                });
+
+                Thread.sleep(2000);
+
+                Platform.runLater(() -> {
+                    sbc.AddData("seria6", "5", 70);
+                    sbc.AddData("seria7", "6", 70);
+                    sbc.AddData("seria7", "7", 70);
+                    sbc.AddData("seria7", "8", 70);
+                    sbc.AddData("seria8", "91", 70);
+                });
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+
 
     }
 
